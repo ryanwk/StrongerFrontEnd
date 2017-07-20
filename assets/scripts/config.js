6e4a63ad730384653391190d901524cae9ab1b19
ryan <ryanwkoch@gmail.com>
2017-07-20 19:47:23 -0400

    added userAuth /exercise data buttons
    threw together basic html skeleton. added userAuth /exercise data
    buttons along with modals. Took these from tictactoe game and left id's
    will use tic tac toe html/css/and ui as a reference when trying to
    make these buttons work for exercise app

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,8 @@
 
 const config = {
   apiOrigins: {
-    production: 'https://ga-wdi-boston.herokuapp.com'
+    production: 'https://stronger.herokuapp.com/',
+    development: 'http://localhost:7165/'
   }
 }
 
